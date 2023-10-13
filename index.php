909632d06bb8033af4568d1b3210c9f7ffd6bcf1
paintedwalls <sandalgunungriki@gmail.com>
2023-10-13 15:32:04 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus magnam explicabo nesciunt dicta quae error dolorum commodi laudantium itaque quo, fugiat accusamus eos alias eius, omnis, reiciendis vel sunt quaerat.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
